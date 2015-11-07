ec1bbdb36f818cd5d697a091f996aaf58e964fbd
Luis Valdes <luisvaldes88@gmail.com>
2015-11-07 16:26:55 -0300

    Agregado http-proxy-middleware, el proyecto esta funcionando correctamente en un stack multipack rails y nodejs.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express');
+var proxyMiddleware = require('http-proxy-middleware');
+
+var proxy = proxyMiddleware('/api', { target: 'http://localhost:5000' });
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(proxy);
 app.use(express.static(__dirname + '/ng-akuerapp/ng-public'));
 
 app.get('/', function(request, response) {
   response.render('index.html');
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
