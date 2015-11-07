37ff17fe586a37215f576a8cc0ff130b332437f8
Luis Valdes <luisvaldes88@gmail.com>
2015-11-07 15:18:38 -0300

    Esta funcionando correctamente en heroku local. Falta importar banco de datos en postgres y todo el ambiente estara listo.

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,13 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 
-require 'fileutils'
-
 # require "active_record/railtie"
 # require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require 'fileutils'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
