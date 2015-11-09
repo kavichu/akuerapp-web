bd651b2ab7c3ec8997dda0edf7ebe898bea611fb
Luis Valdes <luisvaldes88@gmail.com>
2015-11-09 17:52:49 -0300

    Implementado buildsteps para que Gulp pueda generar los archivos y que NodeJS pueda hacer serve del directorio "dist".

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 worker: bundle exec rails s -e production -p 5000
-web: node index.js
+web: sh -c 'cd ng-akuerapp && gulp build && cd ../ && node index.js' 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var proxyMiddleware = require('http-proxy-middleware');
 
 var proxy = proxyMiddleware('/api', { 
-  target: 'http://' + process.env.API_HOST + ':5000' 
+  target: 'http://' + (process.env.API_HOST || 'localhost') + ':5000' 
 });
 
 var app = express();
@@ -10,7 +10,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5100));
 
 app.use(proxy);
-app.use(express.static(__dirname + '/ng-akuerapp/ng-public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(request, response) {
   response.render('index.html');
