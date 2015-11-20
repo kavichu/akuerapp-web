044f6ae62514291edbcee9764851d1b71ac8844f
Luis Valdes <luisvaldes88@gmail.com>
2015-11-20 19:40:58 -0300

    Conditional proxy server.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var proxyMiddleware = require('http-proxy-middleware');
 
-var proxy = proxyMiddleware('/api', { 
-<<<<<<< HEAD
-  target: 'http://' + (process.env.API_PROXY_HOST || 'localhost') + ':5000' 
-=======
-  target: 'http://' + (process.env.API_HOST || 'localhost') + ':5000' 
->>>>>>> 7b2b2f7735c415314197df308b74a3824c89f808
-});
+var proxy = null;
+
+if(process.env.API_PROXY_HOST){
+  proxyMiddleware('/api', { 
+    target: 'http://' + (process.env.API_PROXY_HOST || 'localhost') + ':5000' 
+  });
+}
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5100));
 
-app.use(proxy);
+if(proxy){
+  app.use(proxy);  
+}
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(request, response) {
